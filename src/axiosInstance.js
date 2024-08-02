4e02aff9ab394bffcb28717268c653aac3c73e1c
Rk1105 <113600780+Rohit11D@users.noreply.github.com>
2024-08-02 14:39:23 +0000

    commit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://7fdkv4-5000.csb.app/api',
+    baseURL: 'https://7fdkv4-5000.csb.app',
     headers: {
         'Content-Type': 'application/json',
     },
